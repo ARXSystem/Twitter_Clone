b42b69e736a00a13c21717f26e63e86fac4f4d6a
A.Sys <ec2-user@ip-172-31-35-52.us-east-2.compute.internal>
2018-05-16 05:54:46 +0000

    fix the SQL database

---
+++ SQL/micropost_DB.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE micropost_DB;
+CREATE TABLE micropost_DB.posts_data (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY ,
+    posts VARCHAR (255),
+    users_id INT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
